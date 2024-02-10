f0b9a3e140cdc80fe2d959e53bd3457df95b6faf
Nikita Ambalov <js.python.st@gmail.com>
2024-02-10 22:06:35 +0300

    commit 18.callback exercise #3

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ setTimeout
 но текстовой лекции было написано, что сразу после КАЖДОЙ макрозадачи движок выполняет все задачи из очереди микрозадач перед тем как выполнить следующую макрозадачу.
 Соответственно вопрос весь код от первой строчки до последней - это считается как ОДНА макрозадача? Потому что после первой макрозадачи Promise кажется что должна выполниться микрозадача Promise resolve, и только затем переходить к следующей макрозадачи слову End. Чуть-чуть сбивает значение слова КАЖДОЙ макрозадачи. Что подразумевается под КАЖДОЙ макрозадачей? Спасибо.
 */
-
+/*
 // Задание #2
 
 function runCode() {
@@ -58,7 +58,7 @@ runCode().then(() => console.log("Zero Promise Invoked"));
 
 // 3. вторым выводится One так как макрозадача
 console.log("One");
-
+*/
 /*
 Итого по заданию #2 в консоль выведится в следующем порядке:
 before promise
@@ -66,4 +66,59 @@ One
 Zero
 Zero Promise
 Zero Promise Invoked
+*/
+
+// Задание #3
+const getData = (callback) => {
+  fetch("https://jsonplaceholder.typicode.com/users/1")
+    //   3. помещается в microtask queue и выполняется после выполнения макрозадач
+    .then((response) => response.json())
+    //   4. помещается в microtask queue и выполняется после выполнения макрозадач
+    .then((user) => {
+      // 5. вторым выводится в консоль Success
+      console.log("Success");
+      //   6. помещается в callback queue и выполняется после выполнения микротасков
+      callback(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+getData(() => {
+  // 2. здесь передается callback и код что ниже начнет выполнятся как код в функции достигнет callback(user)
+  //   6.1. начинает исполнятся callback и третьим в консоль выведится user received
+  console.log("user received");
+  const promise = new Promise((resolve) => {
+    // 7. помещается в callback queue и выполняется через 500 милисекунд и после микротасков
+    setTimeout(() => {
+      // 11. через 500 милисекунд исполняется промис, удаляется из callback queue и переход на пункт 12.
+      resolve("promise resolved");
+    }, 500);
+    // 8. четвертым выводится в консоль in promise
+    console.log("in promise");
+    // 9. помещается в callback queue и выполняется через 400 милисекунд и после микротасков
+    setTimeout(() => {
+      // 10. пятым в консоль выводится last set timeout
+      console.log("last set timeout");
+    }, 400);
+  });
+  //  12. помещается в microtask queue и выполняется после выполнения макрозадач
+  promise.then((result) => {
+    // 13. шестым в консоль выводится promise resolved
+    console.log(result);
+  });
+});
+
+// 1. макрозадача - первым в консоль выведится End
+console.log("End");
+
+/*
+Итого по заданию #3 в консоль будет вывод в следующем порядке:
+End
+Success
+user received
+in promise
+last set timeout
+promise resolved
 */
